c0f290f7e38dd10da2074bc13e5f01a20e9a9366
Tom Bollig <tom.bollig@tu-dortmund.de>
2023-11-26 12:15:07 +0100

    Frequenzplot und Druchführung

---
+++ v302/frequenz.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+f,U = np.genfromtxt("Klirrfaktor.txt",unpack=True)
+x=f/150
+y=U*10**-3
+fig, ax=plt.subplots()
+
+ax.set(
+    xlabel=r"$f/f_0$",
+    ylabel=r"$U/U_s$",
+)
+ax.plot(x,y,"o")
+ax.set_xscale("log")
+
+fig.savefig("frequenzplot.pdf")
