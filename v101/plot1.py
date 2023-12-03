946d7011584ef0a79707b9e8cd2a77bbc2dc0585
Tom Bollig <tom.bollig@tu-dortmund.de>
2023-12-03 12:50:06 +0100

    s

--- v101/plot1.py
+++ v101/plot1.py
@@ -6,7 +6,7 @@ x=a*a*0.0001
 y=(b*b)/25
 
 params, covariance_matrix = np.polyfit(x, y, deg=1, cov=True)
-
+errors = np.sqrt(np.diag(covariance_matrix))
 
 x_plot = np.linspace(0, 0.09)
 
@@ -20,7 +20,13 @@ ax.plot(
     label="Lineare Regression",
     linewidth=1.5,
 )
+ax.set(
+    xlabel=r"$a^2[m^2]$",
+    ylabel=r"$T^2[s^2]$",
+)
 print(params[0])
 print(params[1])
+for name, value, error in zip("ab", params, errors):
+    print(f"{name} = {value:.3f} ± {error:.3f}")
 ax.legend();
 fig.savefig("plot.pdf")
