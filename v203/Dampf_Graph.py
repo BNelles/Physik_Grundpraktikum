f996cb4c3bbbd23e5709966afc979de56fa654f8
Tom Bollig <tom.bollig@tu-dortmund.de>
2023-12-04 08:27:47 +0100

    s

--- v203/Dampf_Graph.py
+++ v203/Dampf_Graph.py
@@ -1,12 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-T,p = np.genfromtxt("Dampfwärme_daten.txt", unpack=True)
+T,p = np.genfromtxt("data2.txt", unpack=True)
 fig, ax=plt.subplots()
+x=np.linspace(100,200,10)
+k,cov1=np.polyfit(T,p,deg=3,cov=True)
+unc=np.sqrt(np.diag(cov1))
+f=np.poly1d(k)
+y=f(x)
 
 ax.set(
     xlabel=r"$T/°C$",
     ylabel=r"$p/bar$",
 )
 ax.plot(T,p,"o")
-plt.show()
+ax.plot(x,y,"r--")
+print(k,unc)
+fig.savefig("pk,lot2.pdf")
