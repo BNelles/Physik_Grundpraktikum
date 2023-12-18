2c3eeb0c599b92b420c20c8151d2c1a6e4bdcc5f
Benedikt Nelles <benedikt.nelles@tu-dortmund.de>
2023-12-18 22:12:08 +0100

    Plot für Präzession

--- v105/plot.py
+++ v105/plot.py
@@ -1,18 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.constants as const
+from uncertainties import ufloat
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+a,b=np.genfromtxt("data1.txt" , unpack=True)
+l,p,ö=const.physical_constants["magn. constant"]
+x=a*l*(195/2)*(0.109**2)/((0.109**2+0.069**2)**(3/2)) 
+y=1/b
 
-fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
-ax1.plot(x, y, label="Kurve")
-ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax1.legend(loc="best")
+params, covariance_matrix=np.polyfit(x,y, deg=1, cov=True)
+errors=np.sqrt(np.diag(covariance_matrix))
+x_plot=np.linspace(0.00075,0.00275)
 
-ax2.plot(x, y, label="Kurve")
-ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax2.legend(loc="best")
+
+fig, ax = plt.subplots(1, 1, layout="constrained")
+ax.plot(x, y, "o", label="Messwerte")
+ax.plot(x_plot,params[0]*x_plot+params[1],label="lineare Regression",linewidth=1)
+ax.set_xlabel(r"$B \mathbin{/} \unit{\tesla}$")
+ax.set_ylabel(r"$\overline{T} \mathbin{/} \unit{\per\second}$")
+ax.legend(loc="best")
+print(params[0])
+print(errors[0])
+
+g=ufloat(params[0],errors[0])
+p=2*np.pi*0.0000375*g 
+print(p)
+
 
 fig.savefig("build/plot.pdf")
