f60ac66460e0b9ae238f950ed6e067ebf9bff773
Benedikt Nelles <benedikt.nelles@tu-dortmund.de>
2024-04-15 15:17:53 +0200

    s

--- v408/plot.py
+++ v408/plot.py
@@ -1,18 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+P_L,P_B,B= np.genfromtxt("data/data1.txt",unpack=True)
+P_G=25
+G=3
+V1=B/G
 
-fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
-ax1.plot(x, y, label="Kurve")
-ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax1.legend(loc="best")
+g=P_L-P_G
+b=P_B-P_L
 
-ax2.plot(x, y, label="Kurve")
-ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax2.legend(loc="best")
+V2=b/g
+V=V2-V1
 
-fig.savefig("build/plot.pdf")
+f=g*b/(g+b)
+
+
+print(V1,V2,V)
+
+# fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
+# ax1.plot(g, y, label="Kurve")
+# ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
+# ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
+# ax1.legend(loc="best")
+# 
+# ax2.plot(x, y, label="Kurve")
+# ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
+# ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
+# ax2.legend(loc="best")
+# 
+# fig.savefig("build/plot.pdf")
