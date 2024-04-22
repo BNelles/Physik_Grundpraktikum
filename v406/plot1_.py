d53cc7c6ae88641f2742936ab33ae9c7d8c4900d
Benedikt Nelles <benedikt.nelles@tu-dortmund.de>
2024-04-22 19:56:16 +0200

    s

---
+++ v406/plot1_.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
+i,y = np.genfromtxt("data/data2.txt",unpack=True)
+
+I=(i-0.32)*10**(-9)
+x=(y-25)*0.001
+
+L=1
+phi=x/L
+l=635*10**(-9)
+
+v=0
+
+def sinc(phi,A_0,b):
+     return ((A_0)**2)*(b**2)*((l/(np.pi*b*np.sin(phi+v)))**2)*((np.sin((np.pi*b*np.sin(phi+v))/l))**2)
+
+    #return  A_0*b*phi
+
+
+    #return ((A_0)**2)*(b**2)*((l/(np.pi*b*(phi+v)))**2)*((np.sin((np.pi*b*(phi+v))/l))**2)
+
+
+
+
+
+
+fig, (ax1) = plt.subplots(1, 1, layout="constrained")
+ax1.plot(phi, I,".", label="Spalt 1")
+#ax1.set_xlabel(r"$x \mathbin{/} \unit{\milli\meter}$")
+#ax1.set_ylabel(r"$Stromstärke \mathbin{/} \unit{\nano\ampere}$")
+ax1.legend(loc="best")
+
+params, covariance_matrix=curve_fit(sinc, phi,I)
+uncertainties=np.sqrt(np.diag(covariance_matrix))
+
+
+x_plot=np.linspace(0,55,1)
+
+print(params, uncertainties)
+print(params[0])
+print(params[1])
+print(uncertainties)
+ax1.plot(phi,sinc(phi,params[0],params[1]))
+plt.show()
+#fig.savefig("build/plot.pdf")
