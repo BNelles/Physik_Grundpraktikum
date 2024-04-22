64cf890a0067644ab8697e4be5df4ba46442cd65
Benedikt Nelles <benedikt.nelles@tu-dortmund.de>
2024-04-22 17:03:05 +0200

    s

--- v406/plot2.py
+++ v406/plot2.py
@@ -3,22 +3,28 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 I,y = np.genfromtxt("data/data3.txt",unpack=True)
+
+I=I-0.32
 x=y-25
-def sinc(X,a,b):
-    return a*(np.sin(b*(X))/(X))**2
+L=1000
+phi=x/L
+def sinc(X,A_0,b):
+    #return A_0^2*b^2*(635/(np.pi*b*phi))^2*(np.sin((np.pi*b*phi)/635))^2
+
+    return A_0^2*b^2*(635/(np.pi*b*np.sin(phi)))^2*(np.sin((np.pi*b*np.sin(phi))/635))^2
 
 fig, (ax1) = plt.subplots(1, 1, layout="constrained")
-ax1.plot(x, I,".k" ,label="Doppelspalt")
+ax1.plot(phi, I,".k" ,label="Doppelspalt")
 #ax1.set_xlabel(r"$x \mathbin{/} \unit{\milli\meter}$")
 #ax1.set_ylabel(r"$Stromstärke des Intensitätsmessgertes \mathbin{/} \unit{\nano\ampere}$")
 ax1.legend(loc="best")
 
-params, covariance_matrix=curve_fit(sinc, x,I, p0=[40,0.1])
+params, covariance_matrix=curve_fit(sinc, phi,I,)
 uncertainties=np.sqrt(np.diag(covariance_matrix))
 
 x_plot=np.linspace(0,55,1)
 
 print(params, uncertainties)
-ax1.plot(x_plot,sinc(x_plot,*params,"-"))
+ax1.plot(x,sinc(x,*params,"-"))
 
 fig.savefig("plot2.pdf")
