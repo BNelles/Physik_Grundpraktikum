59c9344c4bc5244d8ab4752fccd70805e00e1a48
tommalte <tom@bollig-online.de>
2024-04-22 16:57:33 +0200

    s

--- v406/plot.py
+++ v406/plot.py
@@ -5,10 +5,10 @@ I,x = np.genfromtxt("data/data1.txt",unpack=True)
 
 
 fig, (ax1) = plt.subplots(1, 1, layout="constrained")
-ax1.plot(x, I, label="Spalt 1")
-ax1.set_xlabel(r"$x \mathbin{/} \unit{\milli\meter}$")
-ax1.set_ylabel(r"$Stromstärke des Intensitätsmessgertes \mathbin{/} \unit{\nano\ampere}$")
+ax1.plot(x, I,".k" ,label="Spalt 1")
+#ax1.set_xlabel(r"$x \mathbin{/} \unit{\milli\meter}$")
+#ax1.set_ylabel(r"$Stromstärke des Intensitätsmessgertes \mathbin{/} \unit{\nano\ampere}$")
 ax1.legend(loc="best")
 
 
-fig.savefig("build/plot.pdf")
+fig.savefig("plot.pdf")
