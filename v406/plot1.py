d072c7b12d6a03c31954828140ac57faf8476d1a
tommalte <tom@bollig-online.de>
2024-04-22 21:20:17 +0200

    s

--- v406/plot1.py
+++ v406/plot1.py
@@ -1,18 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
+from uncertainties import ufloat
 
 def sinc(X,a,b):
     return a*(np.sin((b*X))/(X))**2
 
-I,y = np.genfromtxt("data/data2.txt",unpack=True)
+i,y = np.genfromtxt("data/data2.txt",unpack=True)
+
+I=i-0.32
 x=(y-25)
 
 fig, (ax1) = plt.subplots(1, 1, layout="constrained")
 ax1.plot(x, I,".k" ,label="Spalt 2")
 #ax1.set_xlabel(r"$x \mathbin{/} \unit{\milli\meter}$")
 #ax1.set_ylabel(r"$Stromstärke des Intensitätsmessgertes \mathbin{/} \unit{\nano\ampere}$")
-ax1.legend(loc="best")
+
 
 
 params, covariance_matrix = curve_fit(sinc, x,I, p0=[80,0.5])
@@ -21,9 +24,13 @@ uncertainties=np.sqrt(np.diag(covariance_matrix))
 x_plot=np.linspace(-27,27,100)
 
 print(params, uncertainties)
-ax1.plot(x_plot,sinc(x_plot,params[0],params[1]),"-")
+ax1.plot(x_plot,sinc(x_plot,params[0],params[1]),"-",label="Ausgleichskurve")
 l=635*10**(-9)
-B=(params[1]*l*10**3)/np.pi
+
+ax1.legend(loc="best")
+
+v=ufloat(params[1],uncertainties[1])
+B=(v*l*10**3)/np.pi
 print(B)
 print((1.5*10**(-4)-B)/(1.5*10**(-4)))
 fig.savefig("plot1.pdf")
--- v406/plot2.py
+++ v406/plot2.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
+from uncertainties import ufloat
 
-I,y = np.genfromtxt("data/data3.txt",unpack=True)
+i,y = np.genfromtxt("data/data3.txt",unpack=True)
+
+I=i-0.32
 x=(y-25)*10**(-3)
+
 def f(X,a,b,c):
     return a*((np.cos(c*X)*np.sin(b*(X)))/(X))**2
 
@@ -11,7 +15,7 @@ fig, (ax1) = plt.subplots(1, 1, layout="constrained")
 ax1.plot(x, I,".k" ,label="Doppelspalt")
 #ax1.set_xlabel(r"$x \mathbin{/} \unit{\milli\meter}$")
 #ax1.set_ylabel(r"$Stromstärke des Intensitätsmessgertes \mathbin{/} \unit{\nano\ampere}$")
-ax1.legend(loc="best")
+
 
 params, covariance_matrix=curve_fit(f, x,I,p0=[1,10,0.1])
 uncertainties=np.sqrt(np.diag(covariance_matrix))
@@ -19,10 +23,15 @@ uncertainties=np.sqrt(np.diag(covariance_matrix))
 x_plot=np.linspace(-0.03,0.03,100)
 
 print(params)
-ax1.plot(x_plot,f(x_plot,params[0],params[1],params[2]),"-")
+ax1.plot(x_plot,f(x_plot,params[0],params[1],params[2]),"-",label="Ausgleichskurve")
 l=635*10**(-9)
-B=(params[1]*l)/np.pi
-C=(params[2]*l)/np.pi
+v=ufloat(params[1],uncertainties[1])
+w=ufloat(params[2],uncertainties[2])
+
+ax1.legend(loc="best")
+
+B=(v*l)/np.pi
+C=(w*l)/np.pi
 print(B,(10**(-4)-B)/(10**(-4)))
 print(C,(4*10**(-4)-C)/(4*10**(-4)))
 fig.savefig("plot2.pdf")
