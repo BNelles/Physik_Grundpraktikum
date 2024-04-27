9d640e5bae455d02ed2582aab71448842b0b5002
Benedikt Nelles <benedikt.nelles@tu-dortmund.de>
2024-04-27 13:05:36 +0200

    Durchführung

--- v406/plot.py
+++ v406/plot.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
+from numpy.polynomial import Chebyshev
 
 I,x = np.genfromtxt("data/data1.txt",unpack=True)
 
@@ -7,8 +9,21 @@ I,x = np.genfromtxt("data/data1.txt",unpack=True)
 fig, (ax1) = plt.subplots(1, 1, layout="constrained")
 ax1.plot(x, I, label="Spalt 1")
 ax1.set_xlabel(r"$x \mathbin{/} \unit{\milli\meter}$")
-ax1.set_ylabel(r"$Stromstärke des Intensitätsmessgertes \mathbin{/} \unit{\nano\ampere}$")
+ax1.set_ylabel(r"$Stromstärke \mathbin{/} \unit{\nano\ampere}$")
 ax1.legend(loc="best")
+
+#def poly(x, a, b, c,d,e):
+#    return a*x^4+b*x^3+c*x^2+d*x+e
+#
+#params, covariance_matrix = curve_fit(poly, x, I)
+#
+#ax1.plot(x, poly(x, *params), "-", label="Sigmoid Fit")
+
+
+#c = Chebyshev.fit(x, I, deg=10)
+#
+#
+#ax1.plot(x, c(x), label="Fit")
 
 
 fig.savefig("build/plot.pdf")
--- v406/plot1.py
+++ v406/plot1.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.polynomial import Chebyshev
+
 
 I,x = np.genfromtxt("data/data2.txt",unpack=True)
 
@@ -10,5 +12,10 @@ ax1.set_xlabel(r"$x \mathbin{/} \unit{\milli\meter}$")
 ax1.set_ylabel(r"$Stromstärke des Intensitätsmessgertes \mathbin{/} \unit{\nano\ampere}$")
 ax1.legend(loc="best")
 
+
+#c = Chebyshev.fit(x, I, deg=10)
+#
+#
+#ax1.plot(x, c(x), label="Fit")
 
 fig.savefig("build/plot1.pdf")
