d6126cfeb71b81a10218240589982680f4eb37c8
Benedikt Nelles <benedikt.nelles@tu-dortmund.de>
2024-05-02 18:31:53 +0200

    s

---
+++ v406/Abweichung.py
@@ -0,0 +1,6 @@
+import numpy as np
+from uncertainties import ulfloat 
+
+e=ufloat(,)
+t=
+A=((e-t)/t)*100
--- v406/plot_.py
+++ v406/plot_.py
@@ -14,7 +14,7 @@ l=635*10**(-9)
 v=1.5*10**(-3)
 
 def sinc(phi,A_0,b):
-     return ((A_0)**2)*(b**2)*((l/(np.pi*b*np.sin(phi+v)))**2)*((np.sin((np.pi*b*np.sin(phi+v))/l))**2)
+     return ((A_0)**2)*((l/(np.pi*np.sin(phi+v)))**2)*((np.sin((np.pi*b*np.sin(phi+v))/l))**2)
 
     #return  A_0*b*phi
 
@@ -28,11 +28,11 @@ def sinc(phi,A_0,b):
 
 fig, (ax1) = plt.subplots(1, 1, layout="constrained")
 ax1.plot(phi, I,".", label="Spalt 1")
-#ax1.set_xlabel(r"$x \mathbin{/} \unit{\milli\meter}$")
-#ax1.set_ylabel(r"$Stromstärke \mathbin{/} \unit{\nano\ampere}$")
+ax1.set_xlabel(r"$x \mathbin{/} \unit{\milli\meter}$")
+ax1.set_ylabel(r"$Stromstärke \mathbin{/} \unit{\nano\ampere}$")
 ax1.legend(loc="best")
 
-params, covariance_matrix=curve_fit(sinc, phi,I)
+params, covariance_matrix=curve_fit(sinc, phi,I) 
 uncertainties=np.sqrt(np.diag(covariance_matrix))
 
 
