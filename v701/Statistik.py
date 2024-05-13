f3b366dbdae90409a04e9126214007b68607cc9f
tommalte <tom@bollig-online.de>
2024-05-13 17:17:18 +0200

    s

--- v701/Statistik.py
+++ v701/Statistik.py
@@ -1,6 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-n,z=np.genfromtxt("data3.txt",unpack=True)
+n,z=np.genfromtxt("data/data3.txt",unpack=True)
 z_m=np.mean(z)
 z_s=np.std(z)
+
+def poisson(x,k):
+    return (x**k/np.prod(range(1,k+1)))*np.exp(-x)
+
+fig, ax=plt.subplots(1,1,layout="constrained")
+
+ax.hist(z,bins=100)
+#bin_centers = z[:-1] + np.diff(z) / 2
+
+
+fig.savefig("stat.pdf")
