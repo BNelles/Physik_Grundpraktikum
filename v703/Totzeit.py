4610998dfb2cf2e37cd83824ce9875dc81aa3b31
Benedikt Nelles <benedikt.nelles@tu-dortmund.de>
2024-05-14 12:27:32 +0200

    Kennlinie Plot

---
+++ v703/Totzeit.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+U,n1,n2=np.genfromtxt("data/data2.txt",unpack=True)
+
--- v703/plot.py
+++ v703/plot.py
@@ -1,18 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+U,N=np.genfromtxt("data/data1.txt",unpack=True)
+LN=np.log(N)
 
-fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
-ax1.plot(x, y, label="Kurve")
-ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
+fig, ax1 = plt.subplots(1, 1, layout="constrained")
+ax1.plot(U, LN, label="Kennlinie")
+ax1.set_xlabel(r"$U \mathbin{/} \unit{\volt}$")
+ax1.set_ylabel(r"$N$")
 ax1.legend(loc="best")
 
-ax2.plot(x, y, label="Kurve")
-ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax2.legend(loc="best")
 
 fig.savefig("build/plot.pdf")
