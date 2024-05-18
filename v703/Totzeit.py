89c6625678c6fa7953407b23cd82dea410bc19b2
Benedikt Nelles <benedikt.nelles@tu-dortmund.de>
2024-05-18 10:40:00 +0200

    Regression und Tabelle glaub ich ich weiß es doch auch nicht man

--- v703/Totzeit.py
+++ v703/Totzeit.py
@@ -1,5 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-U,n1,n2=np.genfromtxt("data/data2.txt",unpack=True)
-
+#U,n1,n2=np.genfromtxt("data/data2.txt",unpack=True)
+U=550
+N1=158675
+N2=142577
+N12=264863
+t=(N1+N2-N12)/(2*N1*N2)
+print(t)
--- v703/plot.py
+++ v703/plot.py
@@ -4,11 +4,20 @@ import numpy as np
 U,N=np.genfromtxt("data/data1.txt",unpack=True)
 LN=np.log(N)
 
-fig, ax1 = plt.subplots(1, 1, layout="constrained")
-ax1.plot(U, LN, label="Kennlinie")
-ax1.set_xlabel(r"$U \mathbin{/} \unit{\volt}$")
-ax1.set_ylabel(r"$N$")
-ax1.legend(loc="best")
+
+params, covariance_matrix = np.polyfit(U[0:-3], LN[0:-3], deg=1, cov=True)
+errors = np.sqrt(np.diag(covariance_matrix))
+
+print(errors)
+
+fig, ax = plt.subplots(1, 1, layout="constrained")
+ax.plot(U, LN,".", label="Kennlinie")
+ax.plot(U,params[0]*U+params[1], label="Ausgleichsgerade")
+ax.set_xlabel(r"$U \mathbin{/} \unit{\volt}$")
+ax.set_ylabel(r"$N$")
+ax.legend(loc="best")
+
+
 
 
 fig.savefig("build/plot.pdf")
