8d88d8f333c837bad7a05d1e70bad45f09df7101
Benedikt Nelles <benedikt.nelles@tu-dortmund.de>
2024-06-03 21:17:03 +0200

    s

--- v500/plot.py
+++ v500/plot.py
@@ -6,12 +6,12 @@ u,a=np.genfromtxt("data/grob.txt",unpack=True)
 U=U*10**(-3)
 
 fig, ax1 = plt.subplots(1, 1, layout="constrained")
-ax1.plot(U, A, label="I")
+ax1.plot(U, A,".", label="I")
 ax1.set_xlabel(r"$U \mathbin{/} \unit{\volt}$")
 ax1.set_ylabel(r"$A \mathbin{/} \unit{\nano\ampere}$")
 ax1.legend(loc="best")
 
-ax1.plot(u, a, label="I/2")
+ax1.plot(u, a,".", label="I/2")
 ax1.legend(loc="best")
 142
 
