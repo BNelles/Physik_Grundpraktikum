45522aeeae4b086b5773f2827deacb23038d750d
Benedikt Nelles <benedikt.nelles@tu-dortmund.de>
2024-06-03 08:09:10 +0200

    Daten

--- v500/plot.py
+++ v500/plot.py
@@ -1,18 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+U,A=np.genfromtxt("data/blau.txt",unpack=True)
+u,a=np.genfromtxt("data/grob.txt",unpack=True)
+U=U*10**(-3)
 
-fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
-ax1.plot(x, y, label="Kurve")
-ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
+fig, ax1 = plt.subplots(1, 1, layout="constrained")
+ax1.plot(U, A, label="I")
+ax1.set_xlabel(r"$U \mathbin{/} \unit{\volt}$")
+ax1.set_ylabel(r"$A \mathbin{/} \unit{\nano\ampere}$")
 ax1.legend(loc="best")
 
-ax2.plot(x, y, label="Kurve")
-ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax2.legend(loc="best")
+ax1.plot(u, a, label="I/2")
+ax1.legend(loc="best")
+142
 
 fig.savefig("build/plot.pdf")
