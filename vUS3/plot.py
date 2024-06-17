ecbbb70678fef9a21436d81dd11eafb4c69d6aba
Benedikt Nelles <benedikt.nelles@tu-dortmund.de>
2024-06-17 21:48:39 +0200

    Schmerzen

--- vUS3/plot.py
+++ vUS3/plot.py
@@ -1,28 +1,114 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-v,f= np.genfromtxt("data/data1.txt",unpack=True)
+v,f1= np.genfromtxt("data/data1.txt",unpack=True)
 
-print(np.cos(90/360*2*np.pi))
-print(v[0:5])
+a1=90-np.rad2deg(np.arcsin(np.sin(np.deg2rad(15))*18/27))
+a2=90-np.rad2deg(np.arcsin(np.sin(np.deg2rad(30))*18/27))
+a3=90-np.rad2deg(np.arcsin(np.sin(np.deg2rad(60))*18/27))
 
-f_15=f[0:5]/np.cos(15)
 
+f1_15=f1[0:5]/(np.cos(np.deg2rad(a1)))
+
+f1_30=f1[5:10]/(np.cos(np.deg2rad(a2)))
+
+f1_45=f1[10:15]/(np.cos(np.deg2rad(a3)))
+
+
+print((np.cos(np.deg2rad(a1))))
+
+
+
+v1=(v[0:5]/(np.pi*0.035**2))*0.1/60
+
+v2=(v[0:5]/(np.pi*0.08**2))*0.1/60
 a=15
 c=1800
-v
 
-dv_cos=2*v0*v/c
 
-fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
-ax1.plot(x, y, label="Kurve")
-ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
+
+
+fig, (ax1) = plt.subplots(1, 1, layout="constrained")
+ax1.plot(v1, f1_15,".", label="7mm & 15°")
+#ax1.plot(v1,v1*params15[0]+params15[1],label="Fit")
+#ax1.set_xlabel(r"$v \mathbin{/} \unit{\meter\per\second}$")
+#ax1.set_ylabel(r"$\frac{\increment \nu}{\cos{\alpha}} \mathbin{/} \unit{\hertz}$")
 ax1.legend(loc="best")
 
-ax2.plot(x, y, label="Kurve")
-ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax2.legend(loc="best")
+ax1.plot(v1, f1_30,".", label="7mm & 30°")
+#ax1.plot(v1,v1*params30[0]+params30[1],label="Fit")
+#ax1.set_xlabel(r"$v \mathbin{/} \unit{\meter\per\second}$")
+#ax1.set_ylabel(r"$\frac{\increment \nu}{\cos{\alpha}} \mathbin{/} \unit{\hertz}$")
+ax1.legend(loc="best")
 
-fig.savefig("build/plot.pdf")
+ax1.plot(v1, f1_45,".", label="7mm & 60°")
+#ax1.plot(v1,v1*params45[0]+params45[1],label="Fit")
+#ax1.set_xlabel(r"$v \mathbin{/} \unit{\meter\per\second}$")
+#ax1.set_ylabel(r"$\frac{\increment \nu}{\cos{\alpha}} \mathbin{/} \unit{\hertz}$")
+ax1.legend(loc="best")
+
+
+
+
+
+
+v2,f2= np.genfromtxt("data/data1.txt",unpack=True)
+
+a1=90-np.rad2deg(np.arcsin(np.sin(np.deg2rad(15))*18/27))
+a2=90-np.rad2deg(np.arcsin(np.sin(np.deg2rad(30))*18/27))
+a3=90-np.rad2deg(np.arcsin(np.sin(np.deg2rad(60))*18/27))
+
+
+f2_15=f1[15:20]/(np.cos(np.deg2rad(a1)))
+
+f2_30=f1[20:25]/(np.cos(np.deg2rad(a2)))
+
+f2_45=f1[25:30]/(np.cos(np.deg2rad(a3)))
+
+
+
+
+
+
+
+
+v2=(v1[0:5]/(np.pi*0.08**2))*0.1/60
+a=15
+c=1800
+
+
+f_all=np.reshape(np.array([f1_15,f1_30,f1_45,f2_15,f2_30,f2_45]),30)
+
+v_1=np.reshape(np.array([v1,v1,v1]),15)
+v_2=np.reshape(np.array([v2,v2,v2]),15)
+print(f_all[0:15])
+params,err=np.polyfit(v_1,f_all[0:15],deg=1,cov=True)
+params2,err2=np.polyfit(v_2,f_all[15:30],deg=1,cov=True)
+params3=(params+params2)/2
+print(params,params2,params3)
+
+
+ax1.plot(v2, f2_15,".", label="16mm & 15°")
+ax1.plot(v1,v1*params3[0]+params3[1],label="Fit")
+#ax1.plot(v2,v2*p2arams15[0]+p2arams15[1],label="Fit")
+#ax1.set_xlabel(r"$v \mathbin{/} \unit{\meter\per\second}$")
+#ax1.set_ylabel(r"$\frac{\increment \nu}{\cos{\alpha}} \mathbin{/} \unit{\hertz}$")
+ax1.legend(loc="best")
+
+ax1.plot(v2, f2_30,".", label="16mm & 30°")
+#ax1.plot(v2,v2*p2arams30[0]+p2arams30[1],label="Fit")
+#ax1.set_xlabel(r"$v \mathbin{/} \unit{\meter\per\second}$")
+#ax1.set_ylabel(r"$\frac{\increment \nu}{\cos{\alpha}} \mathbin{/} \unit{\hertz}$")
+ax1.legend(loc="best")
+
+ax1.plot(v2, f2_45,".", label="16mm & 60°")
+#ax1.plot(v2,v2*p2arams45[0]+p2arams45[1],label="Fit")
+#ax1.set_xlabel(r"$v \mathbin{/} \unit{\meter\per\second}$")
+#ax1.set_ylabel(r"$\frac{\increment \nu}{\cos{\alpha}} \mathbin{/} \unit{\hertz}$")
+ax1.legend(loc="best")
+
+
+#print(p2arams15[0],p2arams30[0],p2arams45[0])
+
+plt.show()
+#fig.savefig("build/plot.pdf")
---
+++ vUS3/profil.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+t,f= np.genfromtxt("data/data2.txt",unpack=True)
+
+t=t-30.7*0.4
+d=t*7/4
+
+a1=90-np.rad2deg(np.arcsin(np.sin(np.deg2rad(15))*18/27))
+
+v0=2*(10**0)
+c=3*(10**2)
+
+v=f/((np.cos(np.deg2rad(a1)))/((2*v0)/(c)))
+
+#a1=90-np.arcsin(np.sin(15/360*2*np.pi)*18/27)*180/np.pi
+
+#f1=f/(np.cos(a1/360*2*np.pi)*2*2*10**6/(3*10**8))
+
+fig, ((ax1,ax2),(ax3,ax4))=plt.subplots(2,2,layout="constrained")
+ax1.plot(d[0:11],v[0:11],"b.",label="10mm & 3l/min")
+#ax1.set_xlabel(r"$D \mathbin{/} \unit{\milli\meter}$")
+#ax1.set_ylabel(r"$v \mathbin{/} \unit{\meter\per\second}$")
+ax1.legend(loc="best")
+
+ax2.plot(d[11:26],v[11:26],"r.",label="10mm & 6l/min")
+#ax2.set_xlabel(r"$D \mathbin{/} \unit{\milli\meter}$")
+#ax2.set_ylabel(r"$v \mathbin{/} \unit{\meter\per\second}$")
+ax2.legend(loc="best")
+
+ax3.plot(d[26:40],v[26:40],"g.",label="7mm & 3l/min")
+#ax3.set_xlabel(r"$D \mathbin{/} \unit{\milli\meter}$")
+#ax3.set_ylabel(r"$v \mathbin{/} \unit{\meter\per\second}$")
+ax3.legend(loc="best")
+
+
+ax4.plot(d[40:54],v[40:54],"y.",label="7mm & 6l/min")
+#ax4.set_xlabel(r"$D \mathbin{/} \unit{\milli\meter}$")
+#ax4.set_ylabel(r"$v \mathbin{/} \unit{\meter\per\second}$")
+ax4.legend(loc="best")
+
+plt.show()
+print(f/((np.cos(np.deg2rad(a1)))/((2*2*10**6)/(3*10**8))))
