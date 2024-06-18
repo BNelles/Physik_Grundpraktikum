c6a29d5aa4ece4d99c7a66c7b4d04eec45c834ca
Benedikt Nelles <benedikt.nelles@tu-dortmund.de>
2024-06-18 15:14:20 +0200

    Durchführung v602

---
+++ Winkel.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+l=1280
+d=201.4
+T=np.arcsin(l/(2*d))
+print(l/(2*d))
--- vUS3/plot.py
+++ vUS3/plot.py
@@ -6,7 +6,10 @@ v,f= np.genfromtxt("data/data1.txt",unpack=True)
 print(np.cos(90/360*2*np.pi))
 print(v[0:5])
 
-f_15=f[0:5]/np.cos(15)
+f7_15=f[0:5]/np.cos(15)
+v7_15=v[0:5]/(np.pi*0.035**2)*0.1/60
+
+
 
 a=15
 c=1800
@@ -15,7 +18,7 @@ v
 dv_cos=2*v0*v/c
 
 fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
-ax1.plot(x, y, label="Kurve")
+ax1.plot(v7_15, f7_15, label="Kurve")
 ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
 ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
 ax1.legend(loc="best")
