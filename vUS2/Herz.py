a5f07a086c8d2893ad168348f177eaeedbd40e8e
Benedikt Nelles <benedikt.nelles@tu-dortmund.de>
2024-07-01 18:56:50 +0200

    Das Herzzeitvolumen (HZV, englisch cardiac output CO) ist das Volumen des Blutes, das pro Zeitspanne vom Herzen gepumpt wird (Volumenstrom). Die übliche Maßeinheit in der Humanmedizin ist Liter pro Minute, wobei dann vom Herzminutenvolumen (HMV), früher auch Minutenvolumen[1] des Herzens,[2] gesprochen wird.[3]
    Das Herzzeitvolumen ist ein Maß für die Pumpleistung des Herzens bezüglich der transportierten Blutmenge.[4] Es sollte nicht mit dem Blutvolumen und auch nicht mit der Strömungsgeschwindigkeit des Blutes im Kreislauf verwechselt werden. Das zur besseren interindividuellen Vergleichbarkeit durch die Körperoberfläche dividierte Herzzeitvolumen heißt Herzindex (kardialer Index; englisch cardiac index). Erste wissenschaftliche Erwähnung fand das Herzzeitvolumen bei William Harvey, der mit seiner 1628[5] veröffentlichten Berechnung des Halbstundenvolumens[6] einen Beweis für die Existenz des Blutkreislaufs lieferte.

--- vUS2/Herz.py
+++ vUS2/Herz.py
@@ -5,3 +5,18 @@ from uncertainties import ufloat
 h1,t=np.genfromtxt("data/Herz.txt",unpack=True)
 
 h=75-44.21-h1
+
+h_m=ufloat(np.mean(h),np.std(h))*10**(-2)
+
+d=43.7*10**(-2)
+G=np.pi*(d/2)**2
+ESV=1/3*G*h_m
+
+
+t_m=ufloat(np.mean(t),np.std(t))/60
+
+f=1/t_m
+
+HZV=ESV*f
+
+print(HZV)
