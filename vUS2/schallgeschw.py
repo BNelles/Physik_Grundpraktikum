6b23b77a5e69f7362057148ee545910fbd4a19cf
tommalte <tom@bollig-online.de>
2024-07-02 14:37:20 +0200

    s

--- vUS2/schallgeschw.py
+++ vUS2/schallgeschw.py
@@ -15,8 +15,8 @@ errors=np.sqrt(np.diag(covariance_matrix))
 #print(params[0],errors[0])
 x_plot=np.linspace(0,0.00003,100)
 ax.plot(x_plot,params[0]*x_plot+params[1],label="lineare Regression")
-ax.set_xlabel(r"$\increment t_1 \text{/} \unit{\second}$")
-ax.set_ylabel(r"$x_{\symup{A}} \text{/} \unit{\meter}$")
+#ax.set_xlabel(r"$\increment t_1 \text{/} \unit{\second}$")
+#ax.set_ylabel(r"$x_{\symup{A}} \text{/} \unit{\meter}$")
 
 fig.savefig("build/schall.pdf")
 
@@ -26,10 +26,13 @@ xd=ufloat(params[1],errors[1])
 t2=t2/2
 d=d*10**(-3)
 h=80*10**(-3)
-
-x=c*t+xd
+#print((h-x-d)*10**3)
+x1=c*t+xd
 x2=c*t2*10**(-6)+xd
 d2=h-(x+x2)
-#print(x*10**3)
+#print(x1*10**3)
+print((x1-x)/x*100)
 #print(d2*10**3)
 #print((d2-d)/d*100)
+#print((x2)*10**3)
+#print((x2-((h-x-d)))/((h-x-d))*100)
